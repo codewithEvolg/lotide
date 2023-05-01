e5ca56990ec3bf0783e2295aea8f9fd43eff68a6
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-05-01 16:41:34 +0000

    return key that has value

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if(actual === expected){
+    console.log(`${String.fromCodePoint(0x1F354)}  Assertion Passed:  ${actual}   ===  ${expected}`);
+  }
+  else{
+    console.log(`${String.fromCodePoint(0x1F621)}  Assertion Failed:  ${actual}   !==  ${expected}`);
+  }
+};
+
+const findKeyByValue = (obj, val) => {
+  for(const item in obj){
+    if(obj[item] === val){
+      return item;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+findKeyByValue(bestTVShowsByGenre, "The Wire");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
