33f16ba9cae12e212f7c5dcd5c54b7e5e675beb2
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-05-01 00:54:38 +0000

    adding function to count letters in a sentence

---
+++ countLetters.js
@@ -0,0 +1,34 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if(actual === expected){
+    console.log(`${String.fromCodePoint(0x1F354)}  Assertion Passed:  ${actual}   ===  ${expected}`);
+  }
+  else{
+    console.log(`${String.fromCodePoint(0x1F621)}  Assertion Failed:  ${actual}   !==  ${expected}`);
+  }
+};
+
+const countOccurrence = (arr, item)=>{
+  //count occurrence of element
+  counter = 0;
+  for(const a of arr){
+    if(a === item){
+      counter += 1; 
+    }
+  }
+  return counter;
+}
+
+const countLetters = (sentence) => {
+  finalObj = {};
+  cloneArray = [];
+  for(const element of sentence){
+    element === " " ? element : cloneArray.push(element);
+  }
+  for(const item of cloneArray){
+    finalObj[item] = countOccurrence(cloneArray, item);
+  }
+  console.log(finalObj);
+}
+
+countLetters('The boy is going to the supermarket');
