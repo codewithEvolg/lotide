2c805240e4741872646b1154de214d173e2ddee6
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-05-04 12:03:14 +0000

    created a map function and tested it

---
+++ map.js
@@ -0,0 +1,46 @@
+const assertEqual = function(actual, expected) {
+  if(actual === expected){
+    console.log(`${String.fromCodePoint(0x1F354)}  Assertion Passed:  ${actual}   ===  ${expected}`);
+  }
+  else{
+    console.log(`${String.fromCodePoint(0x1F621)}  Assertion Failed:  ${actual}   !==  ${expected}`);
+  }
+};
+
+const assertArraysEqual = function(array1, array2){
+  let result = false;
+  if(array1.length !== array2.length){
+    return false;
+  }
+  else{
+    for(let a in array1){
+      if(array1[a] !== array2[a]){
+        return false;
+      }
+      else{
+        result = true;
+      };
+    }
+  }
+  return result;
+}
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+
+// const results1 = words.map((word) => {
+//   return word[0];
+// });
+console.log(results1);
+
+const testResult = assertArraysEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]);
+console.log(testResult);
