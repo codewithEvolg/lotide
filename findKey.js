365d9656c2ab59f76f2fb66d7807e85b2b729d42
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-05-05 03:20:09 +0000

    findKey of object in an object

---
+++ findKey.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if(actual === expected){
+    console.log(`${String.fromCodePoint(0x1F354)}  Assertion Passed:  ${actual}   ===  ${expected}`);
+  }
+  else{
+    console.log(`${String.fromCodePoint(0x1F621)}  Assertion Failed:  ${actual}   !==  ${expected}`);
+  }
+};
+
+
+const findKey = (obj, callback) => {
+  for(const item in obj){
+    for(const star in obj[item]){
+      if(callback(obj[item])){
+        //console.log(item);
+        return item;
+      }
+    }
+  }
+}
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
