c0bcac738cb1bf0af9bb82e5c013651ad3577229
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-05-05 02:51:46 +0000

    do a task until using a callback function

---
+++ takeUntil.js
@@ -0,0 +1,55 @@
+const assertEqual = function(actual, expected) {
+  if(actual === expected){
+    console.log(`${String.fromCodePoint(0x1F354)}  Assertion Passed:  ${actual}   ===  ${expected}`);
+  }
+  else{
+    console.log(`${String.fromCodePoint(0x1F621)}  Assertion Failed:  ${actual}   !==  ${expected}`);
+  }
+};
+
+const assertArraysEqual = function(array1, array2){
+  let result = false;
+  if(array1.length !== array2.length){
+    return false;
+  }
+  else{
+    for(let a in array1){
+      if(array1[a] !== array2[a]){
+        return false;
+      }
+      else{
+        result = true;
+      };
+    }
+  }
+  return result;
+}
+
+
+const takeUntil = function(array, callback) {
+  const newArray = [];
+  for(const item of array){
+    if(callback(item)){
+      break;
+    }
+    newArray.push(item);
+  }
+  
+  return newArray;
+}
+
+
+
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+console.log(assertArraysEqual(takeUntil(data1, x => x < 0), [ 1, 2, 5, 7, 2 ]));
