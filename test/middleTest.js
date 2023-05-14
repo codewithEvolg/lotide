e69fa45568e93d029ce50ef216cfca8974ef94f5
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-05-14 02:47:43 +0000

    require index.js file to access every exported function

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,17 +1,18 @@
 const assert = require('chai').assert;
-const middle   = require('../middle');
+const _ = require('././../index')
+//const middle   = require('../middle');
 
 describe("#middle", () => {
   it("returns [2] for [1, 2, 3]", () => {
-    assert.deepEqual(middle([1, 2, 3]), [2]);
+    assert.deepEqual(_.middle([1, 2, 3]), [2]);
   });
 
   it("returns [4,5] for [1, 2, 3, 4, 5, 6, 7, 8]", () => {
-    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [ 4, 5]);
+    assert.deepEqual(_.middle([1, 2, 3, 4, 5, 6, 7, 8]), [ 4, 5]);
   });
 
   it("returns empty array for [5]", () => {
-    assert.deepEqual(middle([5]), []); 
+    assert.deepEqual(_.middle([5]), []); 
   });
 });
 
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,17 +1,17 @@
 const assert = require('chai').assert;
-const tail   = require('../tail');
+const _ = require('././../index')
 
 describe("#tail", () => {
   it("returns [2,3] for [1, 2, 3]", () => {
-    assert.deepEqual(tail([1, 2, 3]), [2,3]);
+    assert.deepEqual(_.tail([1, 2, 3]), [2,3]);
   });
 
   it("returns [6,7] for [5,6,7]", () => {
-    assert.deepEqual(tail([5, 6, 7]), [6,7]);
+    assert.deepEqual(_.tail([5, 6, 7]), [6,7]);
   });
 
   it("returns empty array for [5]", () => {
-    assert.deepEqual(tail([5]), []); 
+    assert.deepEqual(_.tail([5]), []); 
   });
 });
 
